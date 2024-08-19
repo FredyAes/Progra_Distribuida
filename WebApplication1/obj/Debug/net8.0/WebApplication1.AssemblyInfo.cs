7e9017a97af3af288917b9fd9371c32b01c76663
AntonioOrtega12 <parking12346@gmail.com>
2024-08-19 09:30:30 -0600

    Hanako

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28ec7be1838219633bd4d993c8466200c545d1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cfa246346fbc4f5151b95ba4d97e0902881682")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
