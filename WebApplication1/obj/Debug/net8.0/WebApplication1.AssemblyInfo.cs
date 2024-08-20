da30a5e0539e5fbd3a38fc5c69323911f61f8092
Luis Mario Hernandez Cantu <luishernandez16141@gmail.com>
2024-08-20 11:58:40 -0600

    HunterXhunter

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cfa246346fbc4f5151b95ba4d97e0902881682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9017a97af3af288917b9fd9371c32b01c76663")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
