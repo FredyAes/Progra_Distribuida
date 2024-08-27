5ec1ba15596272407512c40d104065100c608382
Alfredo Esparza Snyder <alfredoesparza250@gmail.com>
2024-08-27 12:24:00 -0600

    Formulario

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da30a5e0539e5fbd3a38fc5c69323911f61f8092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb114121e410b2ea8bc68e84cef3cce05c48d3ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
