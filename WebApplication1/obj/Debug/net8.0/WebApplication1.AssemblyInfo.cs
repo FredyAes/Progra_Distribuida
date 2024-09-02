67f632bb377630f6e32ac6dcbd262245c827507e
Alfredo Esparza Snyder <alfredoesparza250@gmail.com>
2024-09-02 09:04:46 -0600

    Carrusel de fotos

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb114121e410b2ea8bc68e84cef3cce05c48d3ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec1ba15596272407512c40d104065100c608382")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
